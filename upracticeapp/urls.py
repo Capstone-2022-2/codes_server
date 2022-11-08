459654c114d841f06fd9a1568aca874f7c8c31ee
hyejin <rkdgpwlsz@naver.com>
2022-11-08 14:25:15 +0900

    연습삭제,연습조회수정

--- upracticeapp/urls.py
+++ upracticeapp/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from upracticeapp.views import upractice_main, upractice_first, upractice_second
+from upracticeapp.views import upractice_main, upractice_first, upractice_second, delete_upractice
 
 app_name = 'upracticeapp'
 
 urlpatterns = [
-    path('<int:pk>/', upractice_main, name='mainupractice'),
+    path('', upractice_main, name='mainupractice'),
     path('ucreate/', upractice_first, name='ufirstpractice'),
     path('second/<int:upractice_id>', upractice_second, name='usecondpractice'),
+    path('delete_upractice/', delete_upractice, name='delete_upractice'),
 ]
--- upracticeapp/views.py
+++ upracticeapp/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView
 from upracticeapp.models import Upractice
 
 
-def upractice_main(request, pk):
-    uall = Upractice.objects.filter(writer=pk) # main에서 들어오는 Upractice의 모든 값을 uall에 저장
+def upractice_main(request):
+    uall = Upractice.objects.filter(writer=request.user) # main에서 들어오는 Upractice의 모든 값을 uall에 저장
     context = {'uall': uall}
     return render(request, "upracticeapp/upractice_main.html", context)
 
@@ -28,7 +29,7 @@ def upractice_first(request):
             upractice_data.save()
             print(upractice_data)
 
-            uall = Upractice.objects.all()  # main에서 들어오는 Upractice의 모든 값을 uall에 저장
+            uall = Upractice.objects.filter(writer=request.user)  # main에서 들어오는 Upractice의 모든 값을 uall에 저장
             context = {'uall': uall}
 
             return render(request, "upracticeapp/upractice_main.html", context)
@@ -39,7 +40,14 @@ def upractice_first(request):
     return render(request, "upracticeapp/upractice_first.html")
 
 def upractice_second(request, upractice_id):
-            g = Upractice.objects.get(pk=upractice_id)
-            context = {'g': g}
-            print(g)
-            return render(request, "upracticeapp/upractice_second.html", context)
+        g = Upractice.objects.get(pk=upractice_id)
+        context = {'g': g}
+        print(g)
+        return render(request, "upracticeapp/upractice_second.html", context)
+
+@csrf_exempt
+def delete_upractice(request):
+    print('실행!')
+    print(request.GET['upractice_id'])
+    Upractice.objects.filter(pk=request.GET['upractice_id']).delete()
+    return JsonResponse(data={})
