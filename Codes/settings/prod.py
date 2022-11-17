91e57bfb9e433e5bed0456a83b58954af9ecbcce
dywudmin <mindu6424@naver.com>
2022-11-18 00:54:47 +0900

    prod db 설정

--- Codes/settings/prod.py
+++ Codes/settings/prod.py
@@ -1,8 +1,8 @@
 from .base import *
-
 import environ
 
 ALLOWED_HOSTS = ['15.164.3.60']
+
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
@@ -13,10 +13,10 @@ environ.Env.read_env(BASE_DIR / '.env')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
+        'NAME': env('reviewdb'),
+        'USER': env('dbmasteruser'),
+        'PASSWORD': env('codes123'),
+        'HOST': env('ls-effa51b199b260903146e54b56c8f3687212f536.cudbqywugn2c.ap-northeast-2.rds.amazonaws.com'),
         'PORT': '5432',
     }
 }
