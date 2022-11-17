ee2879d9980a7b4f4f8f7db1b970beb2c60c7f6a
dywudmin <mindu6424@naver.com>
2022-11-18 00:36:52 +0900

    prod 수정

--- Codes/settings/prod.py
+++ Codes/settings/prod.py
@@ -1,7 +1,21 @@
 from .base import *
+import environ
 
 ALLOWED_HOSTS = ['15.164.3.60']
 
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / '.env')
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('mainapp'),
+        'USER': env('dbmasteruser'),
+        'PASSWORD': env('codes123'),
+        'HOST': env('ls-effa51b199b260903146e54b56c8f3687212f536.cudbqywugn2c.ap-northeast-2.rds.amazonaws.com'),
+        'PORT': '5432',
+    }
+}
