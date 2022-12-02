c77da730f85ebb13bf57e765a4ca7b9cdc6416e9
Hyejin <93965930+hyejin0916@users.noreply.github.com>
2022-12-02 15:42:58 +0900

    공지사항 제한

--- postapp/forms.py
+++ postapp/forms.py
@@ -6,9 +6,8 @@ from postapp.models import Post
 class PostCreationForm(ModelForm):
     class Meta:
         model = Post
-        fields = ['category', 'post_title', 'image', 'post_content']
+        fields = ['post_title', 'image', 'post_content']
         labels = {
-            'category': '카테고리',
             'post_title': '제목',
             'image': '이미지',
             'post_content': '내용'
