530f8bae3f3260e0eb684e3fec7af710516b0c65
dywudmin <mindu6424@naver.com>
2022-12-04 02:07:38 +0900

    중복저장 alert

--- Codes/urls.py
+++ Codes/urls.py
@@ -29,7 +29,9 @@ urlpatterns = [
     path('profiles/', include('profileapp.urls')),
     path('comments/', include('commentapp.urls')),
     path('game/', include('gameapp.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
